2e5ebd628631cb64ace63ed424c47435d4efa8ab
Paul Jung (Thanat0s) <Paul.Jung@circl.lu>
2025-09-23 12:09:22 +0200

    change name in menu

--- webapp/app/security.py
+++ webapp/app/security.py
@@ -1,25 +1,26 @@
-'''
+"""
 This module implemenets additional item in the security menu.
-'''
+"""
 
 from flask_appbuilder.security.sqla.manager import SecurityManager
-from .models import ApiKeys
+
 
 class CustomSecurityManager(SecurityManager):
-    '''
-        Class to implement shortcut in security folder
-    '''
+    """
+    Class to implement shortcut in security folder
+    """
+
     def __init__(self, appbuilder):
         super().__init__(appbuilder)
         self.add_custom_menu_item()
 
     def add_custom_menu_item(self):
-        '''
-        Add Apikey to menu    
-        '''
+        """
+        Add Apikey to menu
+        """
         self.appbuilder.menu.add_link(
-            "ApiKeys",  # Nom de l’item
+            "Agent Keys",  # Nom de l’item
             href="/apikeysview/list/",
             category="Security",
-            icon="fa-folder-open-o"
+            icon="fa-folder-open-o",
         )
