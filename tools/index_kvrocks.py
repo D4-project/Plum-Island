807a976c15589870ee01c328be6d904b18994da1
Paul Jung (Thanat0s) <Paul.Jung@circl.lu>
2026-01-27 15:42:46 +0100

    add tlds whitlisting to import

--- tools/index_kvrocks.py
+++ tools/index_kvrocks.py
@@ -1,9 +1,9 @@
 #!/bin/env python
-'''
-This script will import json data exported from meilidb
+"""
+This script will import json data exported from meilidb export
 into the Kvrocks for idexation.
 
-'''
+"""
 import os
 import json
 import sys
@@ -12,11 +12,13 @@ import yaml
 sys.path.append("../webapp/app/utils")  # parent of webapp
 from kvrocks import KVrocksIndexer
 from result_parser import parse_json
+from mutils import fetch_tlds
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 KVROCKS_PORT = config.get("OUT_KVROCKS_PORT")
 KVROCKS_HOST = config.get("OUT_KVROCKS_HOST")
+
 
 def json_import(json_file):
     print(f"Importing {json_file}")
@@ -29,6 +31,7 @@ INPUT_DIR = "meili_dump"
 indexer = KVrocksIndexer(KVROCKS_HOST, KVROCKS_PORT)
 max_per_folder = 10000
 max_per_folder = 1
+tlds = fetch_tlds()
 for folder in "abcdef0123456789":
     objects_to_index = []
     path = os.path.join(INPUT_DIR, folder)
@@ -36,7 +39,7 @@ for folder in "abcdef0123456789":
         if file.endswith(".json"):
             objects_to_index.append(json_import(os.path.join(INPUT_DIR, folder, file)))
             if len(objects_to_index) == max_per_folder:
-                indexer.add_documents_batch(objects_to_index)
+                indexer.add_documents_batch(objects_to_index, tlds)
                 objects_to_index = []
     # finally
     indexer.add_documents_batch(objects_to_index)
